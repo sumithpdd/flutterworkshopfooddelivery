535ecbb730171ad35b91c55d9f8186d73ad0ded4
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-10-21 13:22:57 +0200

    Search with Recent Orders

--- food_delivery/lib/screens/home_screen.dart
+++ food_delivery/lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/data/data.dart';
+import 'package:food_delivery/helpers/style.dart';
+import 'package:food_delivery/widgets/recent_orders.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -28,6 +30,36 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           )
+        ],
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                  fillColor: white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(width: 0.8),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(
+                        width: 0.8, color: Theme.of(context).primaryColor),
+                  ),
+                  hintText: 'Search Food or Restaurants',
+                  prefixIcon: Icon(
+                    Icons.search,
+                    size: 30.0,
+                  ),
+                  suffixIcon:
+                      IconButton(icon: Icon(Icons.clear), onPressed: () {})),
+            ),
+          ),
+          RecentOrders(),
         ],
       ),
     );
