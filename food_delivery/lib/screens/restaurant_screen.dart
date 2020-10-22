d756d828e136d1b901308f9ba2ec175a85c671da
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-10-22 08:01:16 +0200

    Hero animation to restaurant_screen

--- food_delivery/lib/screens/restaurant_screen.dart
+++ food_delivery/lib/screens/restaurant_screen.dart
@@ -12,6 +12,47 @@ class RestaurantScreen extends StatefulWidget {
 class _RestaurantScreenState extends State<RestaurantScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Hero(
+                tag: widget.restaurant.imageUrl,
+                child: Image(
+                  height: 220.0,
+                  width: MediaQuery.of(context).size.width,
+                  image: AssetImage(
+                    widget.restaurant.imageUrl,
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                  vertical: 20.0,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                        icon: Icon(Icons.arrow_back_ios),
+                        color: Colors.white,
+                        iconSize: 30.0,
+                        onPressed: () => Navigator.pop(context)),
+                    IconButton(
+                        icon: Icon(Icons.favorite),
+                        color: Theme.of(context).primaryColor,
+                        iconSize: 35.0,
+                        onPressed: () {})
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
